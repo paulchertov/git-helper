f16fe8f8f10c449a44b4ecf9ced90a01d0d7c60b
paulchertov <paulchertov@gmail.com>
2017-07-15 10:35:45 +0300

    fixed glitch with files parsing

--- config.py
+++ config.py
@@ -6,6 +6,9 @@ WIN_ENCODING = "cp866"  # standard console encoding
 GIT_NOT_TRACKED_MARKER = """
 (use "git checkout -- <file>..." to discard changes in working directory)
 """.strip()  # standard git string before new/updated files section
+GIT_TRACKED_NO_COMMIT_MARKER = """
+(use "git add <file>..." to include in what will be committed)
+""".strip()  # standard git string before new/updated files section when not added any commits
 GIT_TRACKED_MARKER = """
 (use "git reset HEAD <file>..." to unstage)
 """.strip()  # standard git string before new/updated files already added to commit
